dffb5c36a13e41ee4f4b0c5dc0d6b7aa24494f41
SirEatsALotOff <sireatsalot@infinityminers.com>
2017-07-25 16:14:56 -0500

    Fixed bugs, added read (SLATE)

--- Slate/SlateLogger.bat
+++ Slate/SlateLogger.bat
@@ -1,5 +1,7 @@
 @echo off
 title Slate Logger
+
+:main
 set txtmcount=0
 for %%x in (*.txt) do set /a txtmcount+=1
 set textcount=0000
@@ -7,7 +9,6 @@ set /a txtcount= %txtmcount%+1
 IF %txtmcount% GEQ 1001 (
 set /a "textcount=%txtmcount%-1000"
 )
-:main
 cls
 set ordered=%textcount%.%txtcount%
 set pagetext=" "
@@ -15,7 +16,7 @@ set ID="Default"
 echo Slate Logger
 echo There are currently %txtmcount% logs.
 set /p ID=Specific Name or ID ~ 
-if exist C:\SEAL\Slate\%ID%-%ordered%.txt goto main
+if exist C:\SEAL\Slate\%ID%-%ordered%.txt goto read
 if NOT exist  C:\SEAL\Slate\%ID%-%ordered%.txt goto logg
 goto main
 :logg
@@ -23,4 +24,12 @@ set /p pagetextt= Log ~
 set pagetext=%DATE% - %pagetextt%
 @echo>C:\SEAL\Slate\%ID%-%ordered%.txt
 @echo %pagetext%> %ID%-%ordered%.txt
+goto main
+:read
+cls
+echo File already exists:
+echo.
+type C:\SEAL\Slate\%ID%-%ordered%.txt
+echo.
+pause
 goto main
