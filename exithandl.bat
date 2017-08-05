d034e0313e94d5aa0dbebaf4f8f3a870b3dd92b3
Sir Eats A Lot <sireatsalot@infinityminers.com>
2017-08-05 17:00:59 -0500

    Fixed exithandl.bat
    Now it actually does what it's supposed to do.

--- exithandl.bat
+++ exithandl.bat
@@ -1,8 +1,12 @@
 @echo off
-for /f "skip=1" %%G IN (main.config) DO if not defined line set "lin e=%%G"
-Set/A ress=line+1
-call :RL RL 3 "main.config" "%ress%"
+:tag
+for /f "skip=50" %%G IN (main.config) DO if not defined line set "line=%%G"
+Set /A ress=%line%+1
+call :TEXTMAN RL 51 "main.config" "%ress%"
 goto :eof
+:TEXTMAN
+(SET /A "A=0", "LINE=0", "TOTAL_LINES=0")  &  (CALL :%~1 %* || (ECHO Invalid parameter & Exit /B 1)) & (GOTO:EOF)
+
 :RL
 (For /F "tokens=1* delims=]" %%A in ('type "%~3" ^| find /n /v ""') DO (Call Set /A "LINE+=1" && (CMD /C "IF NOT "%%LINE%%" EQU "%~2" (if "%%B" EQU "" (Echo+>> "%~3.NEW") ELSE ((Echo %%B)>> "%~3.NEW")) ELSE ((Echo %~4)>> "%~3.NEW")"))) && (CALL :RENAMER "%~3") & (GOTO:EOF)
 :RENAMER
